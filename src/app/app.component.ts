753f344dccd02c9171452f1c5f5c5ee570e6e2f1
Samuel <samueloluwatommzy@gmail.com>
2018-04-18 01:54:36 +0100

    THe root app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { UIROUTER_DIRECTIVES } from '@uirouter/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Headlines';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule, JsonpModule , Http } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { NewsComponent } from './news/news.component';
+import { SourcesListComponent } from './sources-list/sources-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { SourceHeadlineComponent } from './source-headline/source-headline.component';
+import { CountryHeadlineComponent } from './country-headline/country-headline.component';
+
+import { NewsApiService } from './news-api.service';
+import { UIRouterModule } from '@uirouter/angular';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { UIView } from '@uirouter/angular';
+import { StateService, Transition } from "@uirouter/angular";
+import { RouterModule } from  './router.module';
+import { Location } from'@angular/common';
+
+import { CheckForUpdateService } from './check-for-update.service';
+import { LogUpdateService } from './log-update.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsComponent,
+    SourcesListComponent, 
+    FooterComponent,
+    SourceHeadlineComponent, 
+    CountryHeadlineComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    FormsModule,
+    HttpModule,
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+     CheckForUpdateService,
+     LogUpdateService,
+     NewsApiService,
+     Location
+  ],
+  bootstrap: [UIView]
 })
 export class AppModule { }
